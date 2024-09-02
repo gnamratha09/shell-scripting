0a3211051dd2e12a7d139340a5bb366f2ea8b378
namratha <gummadinamratha@gmail.com>
2024-09-02 19:26:39 -0400

    hello world

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,10 +1,15 @@
-#!/bin/bang
+# #!/bin/bash
+
+# echo "hello world"
+# echo "apple"
+# echo "orange"
+# echo "pineapple"
+# echo "banana"
+# echo "cheery"
+# echo "blueberry"
+# echo "strawberry"
+
+
+#!/bin/bash
 
 echo "hello world"
-echo "apple"
-echo "orange"
-echo "pineapple"
-echo "banana"
-echo "cheery"
-echo "blueberry"
-echo "strawberry"
