b12b3877abe2c54e832dbbd14562219f9b2aceca
namratha <gummadinamratha@gmail.com>
2024-09-03 15:47:08 -0400

    add

---
+++ 08.data-types.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+# NUMBER1=$1
+# NUMBER2=$2
+
+# SUM=$(($NUMBER1+$NUMBER2))
+
+# echo "Total of $NUMBER1 and $NUMBER2 is: $SUM"
+
+SUM=$({1}+{2})
+
+echo "total 0f $1 and $2 is: $SUM" 
