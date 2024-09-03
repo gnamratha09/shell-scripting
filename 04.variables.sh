eb05a3ebf1bffe4beb03245256909eeca1cd6813
namratha <gummadinamratha@gmail.com>
2024-09-03 16:05:22 -0400

    add

--- 04.variables.sh
+++ 04.variables.sh
@@ -6,3 +6,8 @@ echo "$PERSON1:: Hi ${PERSON2}, How are you?"
 echo "${PERSON2}:: Hello $PERSON1. I am fine. How are you doing?"
 echo "$PERSON1:: I am doing good ${PERSON2}. What's going on?"
 echo "${PERSON2}:: I started learning Shell Script $PERSON1"
+
+
+
+echo "hello ${1}"
+echo "hai ${2}"
