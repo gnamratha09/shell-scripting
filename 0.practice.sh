4679d87d146d669f6ceefa5f5189a152c07202e3
namratha <gummadinamratha@gmail.com>
2024-09-03 12:52:38 -0400

    add

--- 0.practice.sh
+++ 0.practice.sh
@@ -10,17 +10,19 @@
 
 # NUMBER=$1
 
-if [ $1 -le 20 ];
+# if [ $1 -le 20 ];
 
-then
-echo "my name is rithvik-naidu. iam from vijayawada"
+# then
+# echo "my name is rithvik-naidu. iam from vijayawada"
 
-else [ $1 -le 30 ];
+# else [ $1 -le 30 ];
 
- echo "my name is namratha. iam rithvik's mom. iam from poranki"
+#  echo "my name is namratha. iam rithvik's mom. iam from poranki"
  
- else
+#  else
 
- echo "my name is ajay. iam from nellore and king follower"
+#  echo "my name is ajay. iam from nellore and king follower"
 
-fi
+# fi
+
+echo "hello nam"
