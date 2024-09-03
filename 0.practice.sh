aa08512bd8be036b7493a84d5da9d03f35106ba4
namratha <gummadinamratha@gmail.com>
2024-09-02 20:03:34 -0400

    hello world

--- 0.practice.sh
+++ 0.practice.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-NAME1=$1
-NAME2=$2
+# NAME1=$1
+# NAME2=$2
 
 
-echo NAME1:"hello ${NAME2}"
-echo NAME2:"Hai ${NAME1}"
+# echo NAME1:"hello ${NAME2}"
+# echo NAME2:"Hai ${NAME1}"
 
+
+NUMBER=$1
+
+if [ $NUMBER - lt 20]
+
+then
+echo "my name is rithvik-naidu. iam from vijayawada"
+
+if [ $NUMBER -gr 20]
+
+then "my name is namratha. iam rithvik's mom. iam from poranki"
+
+fi
