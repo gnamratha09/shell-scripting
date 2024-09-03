f1a1ae2fc5b95684542188ae46e18fb4f103d834
namratha <gummadinamratha@gmail.com>
2024-09-02 20:20:02 -0400

    hello world

--- 0.practice.sh
+++ 0.practice.sh
@@ -15,8 +15,10 @@ if [ $NUMBER - lt 20]
 then
 echo "my name is rithvik-naidu. iam from vijayawada"
 
+fi
+
 if [ $NUMBER -gr 20]
 
-then "my name is namratha. iam rithvik's mom. iam from poranki"
+then echo "my name is namratha. iam rithvik's mom. iam from poranki"
 
 fi
