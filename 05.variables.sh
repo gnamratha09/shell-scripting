0cf0717d31c8a0dfa70563331116fa40b1512254
namratha <gummadinamratha@gmail.com>
2024-09-04 19:20:41 -0400

    add

--- 05.variables.sh
+++ 05.variables.sh
@@ -25,11 +25,11 @@
 
 # echo "hello $1, you are $2 years old and your are from $3".
 
-read -p "enter your name age city" name age city
+# read -p "enter your name age city" name age city
 
-set -- $name $age $city
+# set -- $name $age $city
 
-echo "hello $1, you are $2 years old and your are from $3".
+# echo "hello $1, you are $2 years old and your are from $3".
 
 echo "enter your name city age"
 
