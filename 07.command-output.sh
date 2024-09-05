0356777795a801dadd1facc8b1c73328ac8afe8a
namratha <gummadinamratha@gmail.com>
2024-09-04 21:55:11 -0400

    add

--- 07.command-output.sh
+++ 07.command-output.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-DATE=$(date +%F)
+# DATE=$(date +%F)
 
-echo "Today's date: $DATE"
-# ------------------------------
+# echo "Today's date: $DATE" 
+# # ------------------------------
  
 # DATE=$(date +%F+%H+%M+%S)
 
 # echo "yesterday's date: $DATE"
+
+DATE=$(date)
+
+echo "Today's date: $DATE"
