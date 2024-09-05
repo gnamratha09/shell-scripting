7d7c4488889d25cfcf0cd37f6d1d71af372a7ae2
namratha <gummadinamratha@gmail.com>
2024-09-04 21:04:14 -0400

    add

--- 03.variables.sh
+++ 03.variables.sh
@@ -1,8 +1,16 @@
-#!/bin/bash
-PERSON1=Raheem
-PERSON2=Suresh
+# #!/bin/bash
+# PERSON1=Raheem
+# PERSON2=Suresh
 
-echo "$PERSON1:: Hi ${PERSON2}, How are you?"
-echo "${PERSON2}:: Hello $$PERSON1. I am fine. How are you doing?"
-echo "$PERSON1:: I am doing good ${PERSON2}. What's going on?"
-echo "${PERSON2}:: I started learning Shell Script $PERSON1"
+# echo "$PERSON1:: Hi ${PERSON2}, How are you?"
+# echo "${PERSON2}:: Hello $$PERSON1. I am fine. How are you doing?"
+# echo "$PERSON1:: I am doing good ${PERSON2}. What's going on?"
+# echo "${PERSON2}:: I started learning Shell Script $PERSON1"
+
+#/bin/bash
+
+X=nam
+Y=Mouni
+
+echo "nam: hello ${Y}, good morning"
+echo "$Y: hey ${X}, hey very good morning"
