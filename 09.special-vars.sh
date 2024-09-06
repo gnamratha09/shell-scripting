14ccd0dd378f575c71844ec13149c202aaee8cfc
namratha <gummadinamratha@gmail.com>
2024-09-05 23:49:07 -0400

    add

--- 09.special-vars.sh
+++ 09.special-vars.sh
@@ -10,11 +10,3 @@
 sleep 100 &
 echo "PID of last background command: $!
 
-
-# echo "All variables passed to the script: $@"
-# echo "Number of variables passed: $#"
-# echo "Script name: $0"
-# echo "Current working directory: $PWD"
-# echo "Home directory of current user: $HOME"
-# echo "PID of the script executing now: $$"
-# sleep 100 &
