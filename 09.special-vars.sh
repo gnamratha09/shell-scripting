7f78bad36e19934aa858ce8e25002ac9a6d59cde
namratha <gummadinamratha@gmail.com>
2024-09-05 23:46:04 -0400

    add

--- 09.special-vars.sh
+++ 09.special-vars.sh
@@ -7,7 +7,7 @@
 # echo "Current working directory: $PWD"
 # echo "Home directory of current user: $HOME"
 # echo "PID of the script executing now: $$"
-sleep 100 &
+# sleep 100 &
 # echo "PID of last background command: $!
 
 
@@ -17,3 +17,4 @@ sleep 100 &
 # echo "Current working directory: $PWD"
 # echo "Home directory of current user: $HOME"
 # echo "PID of the script executing now: $$"
+sleep 100 &
